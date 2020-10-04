30d7872341949a7e3f2ded8a83a5e4b8309cd877
Aaron Mc Kenna <mckenna.aaron95@gmail.com>
2020-10-04 19:47:59 +0100

    Completed additional exercises

--- OpenCV_Morphological_Transformations.py
+++ OpenCV_Morphological_Transformations.py
@@ -2,18 +2,27 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
+# "Morphological transformations are simple operations based on the image shape"
+# Normally performed on binary images
+# 2 elements required, original images and a structuring element / Kernel
+
 img = cv2.imread('smarties.png', 0)
 
 _, mask = cv2.threshold(img, 220, 255, cv2.THRESH_BINARY_INV)
 
+# 2 x 2 square
 kernal = np.ones((2, 2), np.uint8)
 
 dilation = cv2.dilate(mask, kernal, iterations=3)
+
 erosion = cv2.erode(mask, kernal, iterations=3)
+
 # Opening = erosion then dilation
 opening = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernal)
+
 # Closing = dilation then erosion
 closing = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernal)
+
 mg = cv2.morphologyEx(mask, cv2.MORPH_GRADIENT, kernal)
 th = cv2.morphologyEx(mask, cv2.MORPH_TOPHAT, kernal)
 
--- OpenCV_smoothing_and_blurring_images.py
+++ OpenCV_smoothing_and_blurring_images.py
@@ -2,15 +2,30 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
+img = cv2.imread('lena.jpg')
+# img = cv2.imread('opencv-logo.png')
+# img = cv2.imread('Screenshot_1.png')
+# img = cv2.imread('fkdjn.jpg')
+# img = cv2.imread('Noise_salt_and_pepper.png')
 
-img = cv2.imread('opencv-logo.png')
 img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-titles = ['image']
-images = [img]
+kernel = np.ones((5, 5), np.float32) / 25
 
-for i in range(1):
-    plt.subplot(1, 1, i+1), plt.imshow(images[i], 'gray')
+dst = cv2.filter2D(img, -1, kernel)
+
+blur = cv2.blur(img, (5, 5))
+gblur = cv2.GaussianBlur(img, (5, 5), 0)
+median = cv2.medianBlur(img, 5)
+# Bilateral Filter good for reducing noise and keeping image sharp
+bilateralFilter = cv2.bilateralFilter(img, 9, 75, 75)
+
+
+titles = ['image', '2D Convolution', 'Blur', 'gBlur', 'Median', 'Bilateral Filter']
+images = [img, dst, blur, gblur, median, bilateralFilter]
+
+for i in range(6):
+    plt.subplot(2, 3, i+1), plt.imshow(images[i], 'gray')
     plt.title(titles[i])
     plt.xticks([]), plt.yticks([])
 
